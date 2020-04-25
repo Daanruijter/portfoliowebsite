6ff1b058182794bb5aa8522b0feb1e07ce9634f1
Daan Ruijter <daan_ruijter@hotmail.com>
2020-04-25 22:29:40 +0200

    created a transformer for the introduction element on the homepage and starting with creating my resume

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -6,7 +6,24 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home-wrapper">
-        Welcome on the web page of full stack web developer Daan Ruijter.
+        <div className="home-introduction">
+          <span class="welcome">
+            WELCOME ON THE WEB PAGE OF THE FULL STACK MERN WEB DEVELOPER DAAN
+            RUIJTER
+          </span>
+          <p>
+            I'm a certified Full Stack MERN web developer. I have built frontend
+            and backend applications with React.js, Vue.js, Vanilla JavaScript,
+            HTML and CSS. <br />
+            This creative process combined with the analytical thinking and
+            having many opportunities to develop myself is what attracts me a
+            lot in web development. <br />I also like the fact that the industry
+            is internationally oriented, because I'm interested in other
+            cultures and languages.
+          </p>
+          Below you can see the projects I worked on. Hover over them for more
+          information and click to go to the project.
+        </div>
         <ITProjects />
       </div>
     );
--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -1,7 +1,65 @@
 import React, { Component } from "react";
+import "../CSS/Resume.css";
 
 export default class Resume extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="resume-wrapper">
+        <div className="resume-top"></div>
+        <div className="resume-left"></div>
+        <div className="resume-right">
+          DAAN LAURENTIUS RUIJTER FULL STACK WEB DEVELOPER <br />
+          I’m very enthusiastic about web development. The combination of
+          analytical challenges, creativity and the international orientation of
+          the profession makes it very re- warding. Being interested in the
+          structure of foreign lan- guages and having worked as an editor
+          before, coding is a logical extension of my capacities. It gives me
+          the oppor- tunity to continually develop myself, which I consider im-
+          portant for my career. I’m open for a position in a motivat- ing
+          environment where I can learn more to contribute to creating valuable
+          web applications. CAREER SUMMARY 12/2019-03/2020 Ubiqum Code Academy
+          Amsterdam Full Stack MERN web developer: Full-time onsite, with the
+          Agile working methodology. I did projects on the base of
+          learning-by-doing. CODING PROJECTS Mytinerary app (best viewed on a
+          mobile device) Created a travel application that shows favourite
+          cities. Set up the backend with Node, Express and MongoDB. For the
+          frontend I used React,js, Redux, combining plain CSS and Materialize.
+          Weather app Achieved making a weather app in Vue.js using Chart.js and
+          a weather API. The app dis- plays the current weather for a particular
+          city. Users can select different weather data to generate forecast
+          charts.
+          <br />
+          Book store app Used an API to dynamically display an array of books
+          with additional information and an image gallery using Vue.js.
+          <br />
+          TGIF: Transparent Government In Fact Created a website using Vanilla
+          JavaScript that dynamically shows API data of the US Congress and
+          Senate. Later I repeated it with Vue.js. The project helped to improve
+          my capacities to refactor code and to adhere to industry convention.
+          <br />
+          NYSL: North Youth League Soccer website Made a youth soccer
+          information website with HTML and CSS, using FlexBox and Grid to
+          position elements. Also applied CSS media queries for responsiveness.
+          <br />
+          09/2013-06/2019 Rodi Media, Purmerend Editor: Edited and published
+          various local news and web articles with a high degree of accuracy. In
+          every self written article I used my own language to make them unique.
+          The interviews I did required good listening and communication skills.
+          01/2014-09/2015 TENNiS.nl, Sassenheim Web editor (voluntary): Wrote
+          and published different news articles about professional national and
+          international tennis matches.
+          <br />
+          12/2012-04/2013 Valkering Media, Amsterdam Internship as an editor for
+          the website Ajax Showtime: (Re)wrote articles about Ajax and soccer in
+          general 07/2012-11/2012 Internet company User.nl, Purmerend Internship
+          marketing department: Developed a media plan to create more company
+          visibility
+          <br />
+          EDUCATION
+        </div>
+        <div className="resume-bottom"></div>
+        <div className="resume-right-line"></div>
+      </div>
+    );
   }
 }

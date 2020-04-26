0c2f63f441f19539b153786d7605a2dd73efa97c
Daan Ruijter <daan_ruijter@hotmail.com>
2020-04-26 18:48:20 +0200

    working on a digital resume

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../CSS/Resume.css";
+import daanfoto from "../pictures/daanfoto.jpeg";
 
 export default class Resume extends Component {
   render() {
@@ -8,26 +9,54 @@ export default class Resume extends Component {
         <div className="resume-top"></div>
         <div className="resume-left"></div>
         <div className="resume-right">
-          DAAN LAURENTIUS RUIJTER FULL STACK WEB DEVELOPER <br />
-          I’m very enthusiastic about web development. The combination of
-          analytical challenges, creativity and the international orientation of
-          the profession makes it very re- warding. Being interested in the
-          structure of foreign lan- guages and having worked as an editor
-          before, coding is a logical extension of my capacities. It gives me
-          the oppor- tunity to continually develop myself, which I consider im-
-          portant for my career. I’m open for a position in a motivat- ing
-          environment where I can learn more to contribute to creating valuable
-          web applications. CAREER SUMMARY 12/2019-03/2020 Ubiqum Code Academy
-          Amsterdam Full Stack MERN web developer: Full-time onsite, with the
-          Agile working methodology. I did projects on the base of
-          learning-by-doing. CODING PROJECTS Mytinerary app (best viewed on a
-          mobile device) Created a travel application that shows favourite
-          cities. Set up the backend with Node, Express and MongoDB. For the
-          frontend I used React,js, Redux, combining plain CSS and Materialize.
-          Weather app Achieved making a weather app in Vue.js using Chart.js and
-          a weather API. The app dis- plays the current weather for a particular
-          city. Users can select different weather data to generate forecast
-          charts.
+          <div className="resume-name">DAAN LAURENTIUS RUIJTER</div>
+          <div className="resume-profession"> FULL STACK WEB DEVELOPER </div>
+          <div className="resume-daanfoto-wrapper">
+            <img className="resume-daanfoto" src={daanfoto} />
+            <div className="resume-introduction">
+              I’m very enthusiastic about web development. The combination of
+              analytical challenges, creativity and the international
+              orientation of the profession makes it very rewarding. Being
+              interested in the structure of foreign languages and having worked
+              as an editor before, coding is a logical extension of my
+              capacities. It gives me the opportunity to continually develop
+              myself, which I consider important for my career. I’m open for a
+              position in a motivating environment where I can learn more to
+              contribute to creating valuable web applications.{" "}
+            </div>
+          </div>
+          <br />
+          <div className="resume-career-summary-heading">
+            <br />
+            CAREER SUMMARY
+            <br />
+            <br />
+            <br />
+          </div>
+          <div className="resume-jobinfo-wrapper">
+            <div className="resume-jobdate">12/2019-03/2020</div>
+            <div className="resume-jobinfo">
+              <div className="resume-bold">Ubiqum Code Academy Amsterdam</div>
+              <div className="resume-normal">
+                Full Stack MERN web developer:
+              </div>
+              <div className="resume-italic">
+                Full-time onsite, with the Agile working methodology. I did
+                projects on the base of learning-by-doing.
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="resume-coding-projects">CODING PROJECTS </div>
+          <br />
+          <br />
+          Mytinerary app (best viewed on a mobile device) Created a travel
+          application that shows favourite cities. Set up the backend with Node,
+          Express and MongoDB. For the frontend I used React,js, Redux,
+          combining plain CSS and Materialize. Weather app Achieved making a
+          weather app in Vue.js using Chart.js and a weather API. The app dis-
+          plays the current weather for a particular city. Users can select
+          different weather data to generate forecast charts.
           <br />
           Book store app Used an API to dynamically display an array of books
           with additional information and an image gallery using Vue.js.
@@ -56,6 +85,37 @@ export default class Resume extends Component {
           visibility
           <br />
           EDUCATION
+          <br />
+          12/2019-03/2020 Full Stack MERN Web development at Ubiqum Code Academy
+          09/2008-11/2011 Master of Journalism at the Vrije Universiteit in
+          Amsterdam 09/2005-07/2007 Master of Economics at the Universiteit van
+          Amsterdam 09/2002-09/2005 Bachelor of Economics at the Universiteit
+          van Amsterdam 08/1996-06/2002 Gymnasium with Latin and Greek at Het
+          Zaanlands Lyceum
+          <br />
+          ACHIEVEMENTS
+          <br />
+          11/2011-03/2014 Camping de Duinvoet, Schoorl Wrote a book about
+          Campung de Duunvoett a small campsute on the foothulls of the Schoorl
+          dunes
+          <br />
+          LANGUAGE COURSES
+          <br />
+          From 2009 I followed Spanish language courses in Barcelona, Valencia
+          and Málaga and Italian courses in Amsterdam. In addition I often visit
+          language meetups to practice lan- guages and to meet new people. Being
+          able to communicate with people, to read, write and listen in other
+          languages is very stimulating for me. The IT world is internationally
+          ori- ented, so I think my knowledge of foreign languages and interest
+          in other cultures could be valuable.
+          <br />
+          OTHER COURSES
+          <br />
+          01/2017-04/2017 Course in literary non fiction with the author Judith
+          Koelemeijer 04/2018-07/2018 Photography course Speer Fotografie
+          Amsterdam
+          <br />
+          <br />
         </div>
         <div className="resume-bottom"></div>
         <div className="resume-right-line"></div>
